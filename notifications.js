c1997f64a8636b7fbef3272eef4ebbb95d4b5950
mikekavouras <kavourasm@gmail.com>
2016-03-17 16:04:28 -0500

    vanilla version

--- notifications.js
+++ notifications.js
@@ -1,40 +1,103 @@
-Notifications = {};
-Notifications.observers = {};
-Notifications.addObserver = function(elems, ev) {
-  if (this.observers[ev] === undefined) {
-    this.observers[ev] = [];
-  }
-  for (var i = 0; i < elems.length; i++) {
-    this.observers[ev].push(elems[i]);
-  }
-}
-Notifications.removeObserver = function(elems, ev) {
-  if (this.observers[ev] === undefined) return;
-  var arr = this.observers[ev];
-  var n = this;
-  var remove = function(arr, elems) {
-    if (arr.length === 0) {
-      delete n.observers[ev];
-      return;
+NotificationCenter = {
+
+  observers: {},
+
+  addObserver: function(elem, namespace, action) {
+    if (typeof this.observers[namespace] === 'undefined') {
+      var event = this._createEvent(namespace);
+      this.observers[namespace] = {event: event, items: []};
     }
+    if (!this._data(elem, namespace).present) {
+      elem.addEventListener(namespace, action, false);
+      this.observers[namespace].items.push([elem, action]);
+    }
+  },
+
+  addObservers: function(elems, namespace, action) {
     for (var i = 0; i < elems.length; i++) {
-      for (var j = 0; j < arr.length; j++) {
-        if (arr[j] === elems[i]) {
-          arr.splice(j,1);
-          remove(arr, elems);
-        };
+      this.addObserver(elems[i], namespace, action);
+    }
+  },
+
+  removeObserver: function(elem, namespace) {
+    var data = this._data(elem, namespace);
+    if (data.present) {
+      elem.removeEventListener(namespace, data.action, false);
+      data.items.splice(data.index, 1);
+      if (data.items.length === 0) {
+        delete this.observers[namespace];
       }
     }
+  },
+
+  removeObservers: function(elems, namespace) {
+    for (var i = 0; i < elems.length; i++) {
+      this.removeObserver(elems[i], namespace);
+    }
+  },
+
+  trigger: function(namespace) {
+    object = NotificationCenter.observers[namespace];
+    if (typeof object === 'undefined') return;
+    var items = object.items;
+    for (var i = 0; i < items.length; i++) {
+      items[i][0].dispatchEvent(object.event);
+    }
+  },
+
+  removeAllObservers: function() {
+    this.observers = {};
+  },
+
+  trigger: function(namespace) {
+    object = NotificationCenter.observers[namespace];
+    if (typeof object === 'undefined') return;
+    var items = object.items;
+    for (var i = 0; i < items.length; i++) {
+      items[i][0].dispatchEvent(object.event);
+    }
+  },
+
+  _data: function(elem, namespace) {
+    var object = this.observers[namespace];
+    if (typeof object === 'undefined') {
+      return {present: false};
+    }
+    var items = object.items;
+    var elems = this._elems(items);
+    var idx = elems.indexOf(elem);
+    var elem = elems[idx];
+    var action = this._actions(items)[idx];
+    return {
+      present: idx >= 0,
+      index: idx,
+      action:action,
+      elem: elem,
+      event: object.event,
+      items: items
+    };
+  },
+
+  _elems: function(items) {
+    var elems = [];
+    for (var i = 0; i < items.length; i++) {
+      elems.push(items[i][0]);
+    }
+    return elems;
+  },
+
+  _actions: function(items) {
+    var actions = [];
+    for (var i = 0; i < items.length; i++) {
+      actions.push(items[i][1]);
+    }
+    return actions;
+  },
+
+  _createEvent: function(namespace) {
+    var event = document.createEvent('Event');
+    event.initEvent(namespace, true, true);
+    return event;
   }
-  remove(arr, elems);
-}
-Notifications.trigger = function(ev, params) {
-  if (Notifications.observers[ev] === undefined) return;
-  var elems = Notifications.observers[ev];
-  for (var i = 0; i < elems.length; i++) {
-    $(elems[i]).trigger(ev, params);
-  }
-}
-Notifications.clearAll = function() {
-  this.observers = {};
+
 }
--- script.js
+++ script.js
@@ -1,143 +1,47 @@
-colors = [
-  "darksalmon",
-  "cadetblue",
-  "darkseagreen",
-  "slategray",
-  "darkgray",
-  "sandybrown",
-  "powderblue",
-  "palevioletred",
-  "goldenrod"
-];
+var eventName = 'CustomEvent.net';
 
-function log(message) {
-  var $textarea = $('textarea');
-  var text = $textarea.val();
-  $textarea.val(text + '\n' + message);
-  $textarea.get(0).scrollTop = $textarea.get(0).scrollHeight;
+function addOne() {
+  var box = document.querySelector('.box');
+  NotificationCenter.addObserver(box, eventName, function() {
+    console.log('triggered event', eventName);
+  });
 }
 
-$(document).ready(function() {
-
-  var $firstGroup = $('.box:lt(5)');
-  var $secondGroup = $('.box').not($firstGroup);
-
-  $firstGroup.bind('click', function() {
-    var $self = $(this);
-
-    if ($self.hasClass('bound')) {
-      Notifications.removeObserver($(this), "changeColor");
-      log("Box " + $self.index() + " has unsubscribed from the changeColor: event");
-    } else {
-      log("Box " + $self.index() + " has subscribed to the changeColor: event");
-      Notifications.addObserver($(this), "changeColor");
-    }
-
-    $self.toggleClass('bound')
+function addAll() {
+  var boxes = document.querySelectorAll('.box');
+  NotificationCenter.addObservers(boxes, eventName, function() {
+    console.log('triggered event', eventName);
   });
+}
 
-  $secondGroup.bind('click', function() {
-    var $self = $(this);
+function removeOne() {
+  var box = document.querySelector('.box');
+  NotificationCenter.removeObserver(box, eventName);
+}
 
-    if ($self.hasClass('bound')) {
-      Notifications.removeObserver($(this), "move");
-      log("Box " + $self.index() + " has unsubscribed from the move: event");
-    } else {
-      log("Box " + $self.index() + " has subscribed to the move: event");
-      Notifications.addObserver($(this), "move");
-    }
+function removeAll() {
+  var boxes = document.querySelectorAll('.box');
+  NotificationCenter.removeObservers(boxes, eventName);
+}
 
-    $self.toggleClass('bound')
-  });
+var buttons = document.getElementsByTagName('button');
 
-  $firstGroup.bind('changeColor', function() {
-    var rand = Math.floor(Math.random() * colors.length + 1);
-    $(this).css('background' , colors[rand]);
-  });
+buttons[0].addEventListener('click', function() {
+  addOne();
+}, false);
 
-  $secondGroup.bind('move', function() {
-    var $self = $(this);
-    var translate = parseInt($self.attr('data-translate'), 10);
-    $self.css('-webkit-transform', 'translateY('+translate+'px)');
-    $self.attr('data-translate', translate + 5);
-  });
+buttons[1].addEventListener('click', function() {
+  addAll();
+}, false);
 
-  $('#change').click(function() {
-    Notifications.trigger('changeColor');
-    log('event fired: colorChanged');
-  });
+buttons[2].addEventListener('click', function() {
+  NotificationCenter.trigger(eventName);
+}, false);
 
-  $('#move').click(function() {
-    Notifications.trigger('move');
-    log('event fired: move');
-  });
+buttons[3].addEventListener('click', function() {
+  removeOne();
+}, false);
 
-  $('#show').click(function() {
-    var text = "";
-    var obj = Notifications.observers;
-
-    for (var key in obj) {
-      text += "event: " + key + " has ";
-      if (Notifications.observers[key].length > 0) {
-        var arr = Notifications.observers[key];
-        text += arr.length + " observer";
-        if (arr.length > 1) text += "s";
-      }
-      text += '\n';
-    }
-
-    if ($.trim(text) === "")
-      log("There are 0 observers");
-    else
-      log(text);
-  });
-
-  $(document).bind('keydown', function(e) {
-    var code = e.keyCode;
-    var index = null;
-    switch (code) {
-      case 49:
-        index = 0;
-        break;
-      case 50:
-        index = 1;
-        break;
-      case 51:
-        index = 2;
-        break;
-      case 52:
-        index = 3;
-        break;
-      case 53:
-        index = 4;
-        break;
-      case 54:
-        index = 5;
-        break;
-      case 55:
-        index = 6;
-        break;
-      case 56:
-        index = 7;
-        break;
-      case 57:
-        index = 8;
-        break;
-      case 48:
-        index = 9;
-        break;
-      case 38:
-        $('#change').trigger('click');
-        break;
-      case 40:
-        $('#move').trigger('click');
-        break;
-      default:
-        return;
-    }
-
-    if (index === null) return;
-
-    $('.box').eq(index).trigger('click');
-  });
-});
+buttons[4].addEventListener('click', function() {
+  removeAll();
+}, false);
